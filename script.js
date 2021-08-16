5988e08628afc33a1505b485ae67b89a9d7c0f72
Pranav Balakrishnan <pranav.balakrishnan@gmail.com>
2021-08-16 14:03:57 +0530

    enabled scrolling

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
-
 var mymap = L.map('mapid').setView([13.0827, 80.2707], 13);
+var marker = new L.Marker([13.0827, 80.2707]).addTo(mymap);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHJhbmF2YmFsYWtyaXNobmFuIiwiYSI6ImNrc2Q1Y2hhcDA1dHoyb251czZqNjJ5OXIifQ.GAwEOUtXWEtB58rzohGM4g', {
     maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoicHJhbmF2YmFsYWtyaXNobmFuIiwiYSI6ImNrc2Q1Y2hhcDA1dHoyb251czZqNjJ5OXIifQ.GAwEOUtXWEtB58rzohGM4g'
 }).addTo(mymap);
-alert("In case the website doesnt work,please try again but with you AdBlock disabled.");
+
+
 document.getElementById("submit").addEventListener("click", function(){
   var ip = document.getElementById("ip").value;
   var api_key = "at_jf5ahyZziGKVfOwHb84eWrPL3I4Jc";
@@ -23,10 +26,14 @@ document.getElementById("submit").addEventListener("click", function(){
             document.getElementById("isp").innerHTML=data.isp;
             var lat=data.location.lat;
             var lng=data.location.lng;
-            var marker = L.marker([lat, lng]).addTo(mymap);
+            mymap.removeLayer(marker)
+            marker = new L.Marker([lat, lng]);
+            mymap.addLayer(marker);
             mymap.flyTo([lat, lng]);
             //$("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
          }
      });
   });
 });
+
+alert("In case the website doesnt work,please try again but with AdBlock disabled.");
